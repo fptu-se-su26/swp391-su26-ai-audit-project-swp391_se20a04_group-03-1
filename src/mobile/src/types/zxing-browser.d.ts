33eb5d1826476773ba1565811887ec9b0f74dc11
Lê Tự Minh Quang <171906330+Quang102769@users.noreply.github.com>
2026-05-28 08:41:41 +0700

    DE190478 feat: Update theme and fix business logic for 4 pages; dashboard,appointment, yard, setting

--- src/mobile/src/types/zxing-browser.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@zxing/browser" {
-  const BrowserMultiFormatReader: any;
-  export type IScannerControls = any;
-  export { BrowserMultiFormatReader };
-  export default BrowserMultiFormatReader;
-}
